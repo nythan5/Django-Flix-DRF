ee191f07e683ba468de502d9d50afb57f4cf295a
Gabriel Nathan <121491867+nythan5@users.noreply.github.com>
2024-06-25 22:35:05 -0300

    Finalizando aula 122 | Atualizando Genero

--- genres/urls.py
+++ genres/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'genres'
 
 urlpatterns = [
-    path('', views.list_view, name='list')
+    path('', views.create_list_view, name='create_list'),
+    path('<int:pk>/', views.detail_view, name='detail')
 ]
