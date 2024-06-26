fe3a1654ae10db4d3f5e93b0dc23a979b910c3c0
nythan5 <g.n.d2014@gmail.com>
2024-06-26 15:09:58 -0300

    Finalizando aula 126 | CBV Create&List

--- genres/urls.py
+++ genres/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'genres'
 
 urlpatterns = [
-    path('', views.create_list_view, name='create_list'),
-    path('<int:pk>/', views.detail_view, name='detail')
+    path('', views.GenreCreateListView.as_view(), name='create_list'),
+    # path('<int:pk>/', views.detail_view, name='detail')
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,49 +1,9 @@
-from django.http import JsonResponse
 from .models import Genre
-import json
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListCreateAPIView
+from .serializer import GenreSerializer
 # Create your views here.
 
 
-@csrf_exempt
-def create_list_view(request):
-    if request.method == "GET":
-        genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-        return JsonResponse(data, safe=False)
-
-    elif request.method == "POST":
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre(name=data['name'])
-        new_genre.save()
-        return JsonResponse(
-            {'id': new_genre.id, 'name': new_genre.name},
-            status=201,
-
-        )
-
-
-@csrf_exempt
-def detail_view(request, pk):
-    genre = get_object_or_404(Genre, pk=pk)
-
-    if request.method == "GET":
-        data = {'id': genre.id, 'name': genre.name}
-        return JsonResponse(data, safe=False)
-
-    elif request.method == "PUT":
-        data = json.loads(request.body.decode('utf-8'))
-        genre.name = data['name']
-        genre.save()
-        return JsonResponse(
-            {'id': genre.id, 'name': genre.name},
-            status=201,
-
-        )
-    elif request.method == "DELETE":
-        genre.delete()
-        return JsonResponse(
-            {'message': f'Genero {genre.name} excluido'},
-            status=204,
-        )
+class GenreCreateListView(ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
