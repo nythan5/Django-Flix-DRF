c4cf7591c0e2055db714e39d0d79f505bb5f329d
nythan5 <g.n.d2014@gmail.com>
2024-06-28 13:47:15 -0300

    Finalizando aula 137 | Registrando Review no ADM

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Review
 
 # Register your models here.
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'movie', 'stars')
+    search_fields = ('movie',)
