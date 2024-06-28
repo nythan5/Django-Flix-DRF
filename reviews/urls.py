ebd58ccae91ad78a1fc7ee2c9a579b11361e4e37
nythan5 <g.n.d2014@gmail.com>
2024-06-28 16:22:01 -0300

    Atualizando URLS

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,7 +4,7 @@ from .views import ReviewListCreateView, ReviewRetrieveUpdateDestroyView
 app_name = 'reviews'
 
 urlpatterns = [
-    path('', ReviewListCreateView.as_view(), name='list_create'),
-    path('<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(),
+    path('api/', ReviewListCreateView.as_view(), name='list_create'),
+    path('api/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(),
          name='retrieve_update_destroy')
 ]
