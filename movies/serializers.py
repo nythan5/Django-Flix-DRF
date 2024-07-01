6d23d7d7cbe1ab7069062065cc690e615a8f1911
nythan5 <g.n.d2014@gmail.com>
2024-07-01 11:02:41 -0300

    Finalizando aula 143 | Inserindo Validate no Serializer de Movie

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 from .models import Movie
 
 
@@ -6,3 +6,17 @@ class MovieSerializer(ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    # django entende que funçoes que iniciam com VALIDATE,
+    # sao funcoes que devem ser executadas para realizar validacoes.
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise ValidationError(
+                'A data de lançamento não pode ser anterior a 1990')
+        return value
+
+    def validate_resume(self, value):
+        if len(200) > 200:
+            raise ValidationError(
+                'O resumo nao deve ser maior do que 20 carácteres')
+        return value
