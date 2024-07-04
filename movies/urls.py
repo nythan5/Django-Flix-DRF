75a87a8f174daa1cca4d8bf502b20ff90cbc17bb
nythan5 <g.n.d2014@gmail.com>
2024-07-04 16:57:18 -0300

    Finalizando aula 164 | Endpoint Stats

--- movies/urls.py
+++ movies/urls.py
@@ -1,4 +1,4 @@
-from .views import MovieListCreateView, MovieRetrieveUpdateDestroyView
+from .views import MovieListCreateView, MovieRetrieveUpdateDestroyView, MovieStatsView  # noqa
 from django.urls import path
 
 app_name = 'movies'
@@ -6,6 +6,7 @@ app_name = 'movies'
 urlpatterns = [
     path('', MovieListCreateView.as_view(), name='list_create'),
     path('<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(),
-         name='retrieve_update_destroy')
+         name='retrieve_update_destroy'),
+    path('stats/', MovieStatsView.as_view(), name='status')
 
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,12 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView  # noqa
+from rest_framework.views import APIView
 from .models import Movie
 from rest_framework.permissions import IsAuthenticated
 from .serializers import MovieSerializer
 from project.permissions import GlobalDefaultPermission
+from rest_framework import response, status
+from django.db.models import Count, Avg
+from reviews.models import Review
 
 # Create your views here.
 
@@ -17,3 +21,32 @@ class MovieRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+
+
+class MovieStatsView(APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
+    queryset = Movie.objects.all()
+
+    def get(self, request):
+        total_movies = self.queryset.count()
+
+        # A função annotate adiciona agregações aos valores retornados pelo queryset. # noqa
+        # Aqui, estamos adicionando uma contagem de IDs (Count('id')) para cada grupo de gêneros. # noqa
+        # Basicamente, ele conta quantos filmes existem para cada gênero.
+        movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))  # noqa
+
+        total_reviews = Review.objects.count()
+
+        # Aqui criamos um campo chamado avg_stars que recebe a Media do total de estrelas # noqa
+        # atravês da função Avg e posteriormente como pegamos apenas este campo atraves dos [] # noqa
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']  # noqa
+
+        return response.Response(
+            data={
+                'total_movies': total_movies,
+                'movies_by_genre': movies_by_genre,
+                'total_review': total_reviews,
+                'average_stars': round(average_stars, 1) if average_stars else 0
+            },
+            status=status.HTTP_200_OK
+        )
